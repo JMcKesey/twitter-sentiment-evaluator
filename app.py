1ba2c746573a54c34413c0380020942035c0b283
JMcKesey <jonathan.mckesey@gmail.com>
2024-01-26 14:30:52 -0500

    added api keys and AJAX so my js and python can communicate

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
+import tweepy
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=['POST']) 
+def process(): 
+    data = request.form.get('data')
+    return data
+
+# retrieving bearer token
+with open("twitter_keys.json") as infile:
+    json_obj = json.load(infile)
+    token = json_obj["bearer_token"]
+    client = tweepy.Client(bearer_token=token)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
